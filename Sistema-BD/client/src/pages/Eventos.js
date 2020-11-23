004458de4f607cdca067ed9742dc62cb8150f9f6
gi-armani <giovana_armani@usp.br>
2020-11-23 20:50:03 -0300

    Design da pagina de eventos

--- Sistema-BD/client/src/pages/Eventos.js
+++ Sistema-BD/client/src/pages/Eventos.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import BotaoForm from '../components/BotaoForm';
 import '../pages/styles/Geral.css';
+import '../pages/styles/Tabela.css';
 
 function Eventos() {
 
@@ -17,42 +18,47 @@ function Eventos() {
     useEffect(getEventos, [])
     
         return (
-            <div className="container-tabelas">
+            <div>
+
                 <div className="topo">
-                    <BotaoForm rota="/formEvento"/>
+                    <BotaoForm rota="http://localhost:3000/formEvento/formEvento"/>
                     <h1>Eventos</h1>
-                </div>
-    
-                <table>
-                    <thead>
-                        <tr>
-                            <th>Nome</th>
-                            <th>Edição</th>
-                            <th>Tema</th>
-                            <th>Público Alvo</th>
-                            <th>Patrocinadores</th>
-                            <th></th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            {listaEventos.map(element => { 
+                    </div>
+                
+                        <div className="table-container">
+                        <table>
+                            
+                            <tr>
+                                <th>Nome</th>
+                                <th>Edição</th>
+                                <th>Tema</th>
+                                <th>Público Alvo</th>
+                                <th>Patrocinadores</th>
+                                <th></th>
+                                <th></th>
+                            </tr>
+                        
+                        
+                            
+                                {listaEventos.map(element => { 
                                 return (
                                         <tr>
                                             <td>{element.nome}</td>
                                             <td>{element.edicao}</td>
                                             <td>{element.tema}</td>
                                             <td>{element.publico_alvo}</td>
-                                            <td>Lalalala</td>
-                                            <td><button>Editar</button></td>
-                                            <td><button>Apagar</button></td>
+                                            <td><button className="botaoSecundario">Ver patrocinadores</button></td>
+                                            <td><button className="botaoSecundario">Editar</button></td>
+                                            <td><button className="botaoSecundario">Apagar</button></td>
                                         </tr>
                                 )
                             })}
-                        </tr>
-                    </tbody>
+                        
+                    
                 </table>
+                        
+                        
+                </div>
     
             </div>
         )
