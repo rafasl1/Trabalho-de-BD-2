50ee7f0277c6fa26ba26038f02b524586fca8cf0
gi-armani <giovana_armani@usp.br>
2020-11-23 14:48:35 -0300

    Pega dados de eventos do back e coloca em tabela
    Co-authored-by: Ana Beatriz <abcuelbas@gmail.com>
    Co-authored-by: Rafael Silva de Lima <rafael.sl@usp.br>
    Co-authored-by: Guilherme Balog <guilherme.gardino@usp.br>

--- Sistema-BD/client/src/pages/Eventos.js
+++ Sistema-BD/client/src/pages/Eventos.js
@@ -1,124 +1,61 @@
-import React, {Component, useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from 'axios';
 import BotaoForm from '../components/BotaoForm';
 import '../pages/styles/Geral.css';
 
-class Eventos extends Component {
+function Eventos() {
 
-    const [listaEventos, setListaEventos] = useState([]);
-
-    listaEventos= () => {
-        const [listaEventos, setListaEventos] = useState([]);
-     }
-
-    getEventos = () => {
-        axios.get("http://localhost:3001/Eventos").then((response) => {
-            /* console.log(response.data.rows) */
-            setListaEventos(response.data.rows)
-            /* console.log(listaEventos) */
-            listaEventos.forEach(element => {
-                console.log(element)
-            });
-        });
+    const [listaEventos, setListaEventos] = useState([])
+    
+    const getEventos = async() => {
+        const response = await axios.get("http://localhost:3001/Eventos")
+            
+        console.log('entrou pro back')
+        setListaEventos(response.data.rows)
     }
 
-    listEventos = () => {
-        listaEventos.forEach(element => {
-            return (
-                <>
-                <h1>Teste</h1>
-                    <td>{element.nome}</td>
-                    <td>{element.edicao}</td>
-                    <td>{element.tema}</td>
-                    <td>{element.publico_alvo}</td>
-                    <td>Lalalala</td>
-                    <td><button>Editar</button></td>
-                    <td><button>Apagar</button></td>
-                </>
-            )
-        });
-        
-            /* listaEventos.map((value, index) => {
-                return (
-                    <>
-                    <h1>Teste</h1>
-                        <td key={index}>{value.nome}</td>
-                        <td key={index}>{value.edicao}</td>
-                        <td key={index}>{value.tema}</td>
-                        <td key={index}>{value.publico_alvo}</td>
-                        <td>Lalalala</td>
-                        <td><button>Editar</button></td>
-                        <td><button>Apagar</button></td>
-                    </>
-                )
-            }) */
-        
-    }
-
-    render() {
+    useEffect(getEventos, [])
+    
         return (
             <div className="container-tabelas">
                 <div className="topo">
                     <BotaoForm rota="/formEvento"/>
                     <h1>Eventos</h1>
                 </div>
-                {/* <button onClick={getEventos}>Teste</button> */}
-                {getEventos}
     
                 <table>
-                    <tr>
-                        <th>Nome</th>
-                        <th>Edição</th>
-                        <th>Tema</th>
-                        <th>Público Alvo</th>
-                        <th>Patrocinadores</th>
-                        <th></th>
-                        <th></th>
-                    </tr>
-                    <tr>
-                        {/* listaEventos.forEach(element => {
-                            return (
-                                <>
-                                <h1>Teste</h1>
-                                    <td>{element.nome}</td>
-                                    <td>{element.edicao}</td>
-                                    <td>{element.tema}</td>
-                                    <td>{element.publico_alvo}</td>
-                                    <td>Lalalala</td>
-                                    <td><button>Editar</button></td>
-                                    <td><button>Apagar</button></td>
-                                </>
-                            )
-                        }) */}
-                        {/* listaEventos.map((value, index) => {
-                            return (
-                                <>
-                                <h1>Teste</h1>
-                                    <td key={index}>{value.nome}</td>
-                                    <td key={index}>{value.edicao}</td>
-                                    <td key={index}>{value.tema}</td>
-                                    <td key={index}>{value.publico_alvo}</td>
-                                    <td>Lalalala</td>
-                                    <td><button>Editar</button></td>
-                                    <td><button>Apagar</button></td>
-                                </>
-                            )
-                        }) */}
-                        {listEventos}
-                        {/* <td>Jill</td>
-                        <td>Smith</td>
-                        <td>50</td> */}
-                    </tr>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Edição</th>
+                            <th>Tema</th>
+                            <th>Público Alvo</th>
+                            <th>Patrocinadores</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            {listaEventos.map(element => { 
+                                return (
+                                        <tr>
+                                            <td>{element.nome}</td>
+                                            <td>{element.edicao}</td>
+                                            <td>{element.tema}</td>
+                                            <td>{element.publico_alvo}</td>
+                                            <td>Lalalala</td>
+                                            <td><button>Editar</button></td>
+                                            <td><button>Apagar</button></td>
+                                        </tr>
+                                )
+                            })}
+                        </tr>
+                    </tbody>
                 </table>
     
-                {/* {listaEventos.map((val, key) => {
-                    return <div>{val.nome}</div>
-                })} */}
             </div>
         )
-    }
-
-    
 }
 
 export default Eventos;
