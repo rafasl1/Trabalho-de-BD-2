1470633580e842f891020469b88de6b8212d56d4
Rafael <rafael.sl@usp.br>
2020-12-01 13:28:40 -0300

    c

--- Sistema-BD/server/src/index.js
+++ Sistema-BD/server/src/index.js
@@ -5,12 +5,12 @@ const cors = require('cors');
 //Middlewars
 app.use(cors())
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", ['GET', 'PUT', 'POST', 'DELETE']);
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     next();
-// });
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", ['GET', 'PUT', 'POST', 'DELETE']);
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
 app.use(express.json()); //permite receber dados do cliente em fortato json
 // app.use(express.urlencoded({ extended: false })); //recebe dados por fomularios
 
