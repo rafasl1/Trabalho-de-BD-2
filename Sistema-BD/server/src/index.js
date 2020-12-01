003827274b42baf08b0b0242234e066a4fbe538e
Rafael <rafael.sl@usp.br>
2020-12-01 11:43:20 -0300

    Mudando as rotas aqui

--- Sistema-BD/server/src/index.js
+++ Sistema-BD/server/src/index.js
@@ -6,32 +6,18 @@ const { urlencoded } = require('express');
 
 
 //Middlewars
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Methods", ['GET', 'PUT', 'POST', 'DELETE']);
-//     res.header('Access-Control-Allow-Headers', 'Content-Type');
-//     app.use(cors());
-//     next();
-// });
-
-// app.use(express.json()); //permite receber dados do cliente em fortato json
-// //Rotas
-// app.use(require('./routes'));
-
-app.use(cors());
-app.use(express.json());
-app.use(
-    "/files",
-    express.static(path.resolve(__dirname, "..", "temp", "uploads"))
-);
 app.use((req, res, next) => {
-    console.log("\nMiddlewere\n");
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", ['Content-Type', 'Authorization']);
-    res.header("Access-Control-Allow-Methods", ['GET', 'PUT', 'POST', 'DELETE', 'UPDATE']);
+    res.header("Access-Control-Allow-Methods", ['GET', 'PUT', 'POST', 'DELETE']);
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    app.use(cors());
     next();
 });
+
+app.use(express.json()); //permite receber dados do cliente em fortato json
+//Rotas
 app.use(require('./routes'));
+
 
 const port = process.env.PORT || 3001;
 
--- Sistema-BD/server/src/routes.js
+++ Sistema-BD/server/src/routes.js
@@ -8,24 +8,24 @@ const { getPatrocinio, getPatrocinioPelaEntidade, addPatrocinio, addPatrocinioPe
 router.get('/', (request, response) => response.json({ message: "Bem vinda(o) à API do trabalho" }))
 
 router.get('/Eventos', getEventos);
-router.post('/adicionaEventos', addEventos);
+router.get('/adicionaEventos', addEventos);
 router.get('/eventoLastId', getLastEventoId);
-router.delete('/removeEventos/:id', removeEventos);
-router.put('/updateEventos', updateEventos);
+router.get('/removeEventos/:id', removeEventos);
+router.get('/updateEventos', updateEventos);
 router.get('/eventoEspecifico/:id', getEventoEspecifico);
 
 router.get('/Patrocinadores', getPatrocinadores);
-router.post('/adicionaPatrocinador', addPatrocinador);
+router.get('/adicionaPatrocinador', addPatrocinador);
 router.get('/patrocinadorLastId', getLastPatrocinadorId);
-router.delete('/removePatrocinador/:id', removePatrocinador);
-router.put('/updatePatrocinador', updatePatrocinador);
+router.get('/removePatrocinador/:id', removePatrocinador);
+router.get('/updatePatrocinador', updatePatrocinador);
 router.get('/patrocinadorEspecifico/:id', getPatrocinadorEspecifico);
 
 router.get('/Patrocinio/:id', getPatrocinio);
 router.get('/PatrocinioPelaEntidade/:id', getPatrocinioPelaEntidade);
-router.post('/adicionaPatrocinio', addPatrocinio);
-router.post('/adicionaPatrocinioPelaEntidade', addPatrocinioPelaEntidade);
-router.delete('/removePatrocinio/:evento/:entidade', removePatrocinio);
-router.put('/updatePatrocinio', updatePatrocinio);
+router.get('/adicionaPatrocinio', addPatrocinio);
+router.get('/adicionaPatrocinioPelaEntidade', addPatrocinioPelaEntidade);
+router.get('/removePatrocinio/:evento/:entidade', removePatrocinio);
+router.get('/updatePatrocinio', updatePatrocinio);
 
 module.exports = router;
