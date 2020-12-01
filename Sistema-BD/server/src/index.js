7255feb09707723450ae3fdb00794a524425e75d
Rafael <rafael.sl@usp.br>
2020-12-01 11:31:41 -0300

    Teste de nov

--- Sistema-BD/server/src/index.js
+++ Sistema-BD/server/src/index.js
@@ -3,17 +3,14 @@ const app = express();
 const cors = require('cors');
 const { urlencoded } = require('express');
 
-var corsConfig = {
-    origin: "*",
-    optionsSuccessStatus: 200
-}
+
 
 //Middlewars
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", ['GET', 'PUT', 'POST', 'DELETE']);
     res.header('Access-Control-Allow-Headers', 'Content-Type');
-    app.use(cors(corsConfig));
+    app.use(cors());
     next();
 });
 
