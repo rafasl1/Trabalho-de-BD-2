6db7c479b1bcfeee89bd42b6c6e114c417a107b6
Rafael <rafael.sl@usp.br>
2020-11-30 23:28:59 -0300

    Fazendo uns testes pra ver se o cors para de encher o saco

--- Sistema-BD/server/src/index.js
+++ Sistema-BD/server/src/index.js
@@ -2,12 +2,9 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
-app.use(cors());
-
 //Middlewars
 app.use(cors());
 app.use(express.json()); //permite receber dados do cliente em fortato json
-app.use(express.urlencoded({ extended: false })); //recebe dados por fomularios
 
 //Rotas
 app.use(require('./routes'));
