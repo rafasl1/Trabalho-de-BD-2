93daf88f01fb82f72f88f247a33169406fdce687
gi-armani <giovana_armani@usp.br>
2020-12-01 22:00:52 -0300

    Tentativa com cors 2

--- Sistema-BD/server/src/index.js
+++ Sistema-BD/server/src/index.js
@@ -7,7 +7,7 @@ const cors = require('cors');
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", ['Content-Type', 'Authorization']);
+    
     res.header("Access-Control-Allow-Methods", ['GET', 'PUT', 'POST', 'DELETE']);
     app.use(cors())
     next();
