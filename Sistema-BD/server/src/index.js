d9b8e76bdb4eef6e03250f1421bd662a8f15a73c
gi-armani <giovana_armani@usp.br>
2020-12-01 22:05:15 -0300

    Deu errado e voltei pra como tava antes

--- Sistema-BD/server/src/index.js
+++ Sistema-BD/server/src/index.js
@@ -3,13 +3,12 @@ const app = express();
 const cors = require('cors');
 
 //Middlewars
-
+app.use(cors())
 
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
-    
     res.header("Access-Control-Allow-Methods", ['GET', 'PUT', 'POST', 'DELETE']);
-    app.use(cors())
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
 });
 app.use(express.json()); //permite receber dados do cliente em fortato json
